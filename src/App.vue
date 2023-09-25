102e6a843f08544149228230539b791032956052
Koen De Groot <koen.de.groot@organimmo.be>
2023-09-25 12:15:36 +0200

    added home page view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <HeroSection></HeroSection>
-  <main>
-    <Categories></Categories>
-  </main>
+  <HomePage />
 </template>
 
 <script setup lang="ts">
-import HeroSection from './components/HeroSection.vue'
-import Categories from './components/Categories.vue'
+import HomePage from './views/HomePage.vue'
 </script>
 
-<style scoped lang="scss">
-main {
-  max-width: 1100px;
-  margin-inline: auto;
-}
-</style>
+<style scoped lang="scss"></style>
