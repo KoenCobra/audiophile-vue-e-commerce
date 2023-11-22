7de4b5fea9e049ca674938cab92614ddbe79b8a6
Koen De Groot <koen.de.groot@organimmo.be>
2023-11-22 13:06:55 +0100

    adjusted path of json file

--- src/stores/productStore.ts
+++ src/stores/productStore.ts
@@ -1,19 +1,19 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import axios from 'axios'
-import type { Product } from "@/types/product";
+import type { Product } from '@/types/product'
 
 export const useProductStore = defineStore('product', () => {
- const products = ref<Product[]>([])
- const product = ref<Product>()
+  const products = ref<Product[]>([])
+  const product = ref<Product>()
 
- const getProducts = async () => {
-  products.value = (await axios.get('data.json')).data;
- }
+  const getProducts = async () => {
+    products.value = (await axios.get('/data.json')).data
+  }
 
- const getProduct = async (id: number) => {
-  product.value = products.value.find(p => p.id === Number(id))
- }
+  const getProduct = async (id: number) => {
+    product.value = products.value.find((p) => p.id === Number(id))
+  }
 
- return { products, product, getProducts, getProduct }
+  return { products, product, getProducts, getProduct }
 })
